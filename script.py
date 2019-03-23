4d0d86d80622209463833fce9589546648d0160d
Camilo <ccmil0fr@gmail.com>
2019-03-23 17:49:04 -0400

    updated python script

--- script.py
+++ script.py
@@ -1,18 +1,5 @@
-
 import random
+import math
 
-ran_num = random.randint(0, 10)
-
-print('Guess a random number between 0 and 10')
-num = input('Enter a number: ')
-
-if num == ran_num:
-    print('The number is correct')
-elif num > 10 or num < 0:
-    print('The number should be between 0 and 10!')
-elif num < ran_num:
-    print('The number should be bigger')
-elif num > ran_num:
-    print('The number should be smaller')
-else:
-    print('Error!!!')
+message = "This is my message"
+print(f"{message} lolololol")
